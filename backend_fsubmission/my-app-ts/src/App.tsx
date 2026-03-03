ec4060727404757fe5682864bb3642dafbce4c55
KoheiSakai <denst2k@gmail.com>
2026-03-03 15:20:21 +0900

    delete useState error

--- backend_fsubmission/my-app-ts/src/App.tsx
+++ backend_fsubmission/my-app-ts/src/App.tsx
@@ -3,8 +3,6 @@ import {useState} from "react";
 import Screen from "./chatbox";
 
 function App() {
-  const [content,setContent] = useState("");
-
   return (
     <div className="App">
       <header className="App-header">
